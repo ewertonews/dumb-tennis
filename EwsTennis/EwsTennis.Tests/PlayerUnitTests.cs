baf2e564c0c107edb06ade8e3c0949d08d94c7c0
Ewerton Willams da Silva <ewerton.willams@philips.com>
2021-03-13 16:14:32 -0300

    Adicionado test para player.Serve

---
+++ EwsTennis/EwsTennis.Tests/PlayerUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EwsTennis.Tests
+{
+    public class PlayerUnitTests
+    {
+        [Test]
+        public void ServeShouldReturnNumerFromOneToTwentySeven()
+        {
+            var player = new Player();
+
+            var result = player.Serve();
+
+            Assert.That(result, Is.GreaterThanOrEqualTo(1));
+            Assert.That(result, Is.LessThanOrEqualTo(27));
+        }
+    }
+}
