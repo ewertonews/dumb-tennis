2627148716961be8fe563ca3ae2d61a04ad82f6a
Ewerton Willams da Silva <ewerton.willams@philips.com>
2021-03-13 16:05:53 -0300

    Adicionando restante das propriedades da class Player e mudando tipo do projeto para console

---
+++ EwsTennis/EwsTennis/Enums/PlayerLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EwsTennis.Enums
+{
+    public enum PlayerLevel
+    {
+        BEGINNER = 3,
+        EXPERIENCED,
+        EXPERT
+    }
+}
--- EwsTennis/EwsTennis/Player.cs
+++ EwsTennis/EwsTennis/Player.cs
@@ -1,14 +1,19 @@
 ﻿using EwsTennis.Enums;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EwsTennis
 {
     public class Player
     {
+        public string Name { get; set; }
+        public PlayerLevel Level { get; set; }
+        public int ReachOfHands { get; private set; }
+        public int Position { get; set; }
         public EvenOrOddOption EvenOrOdd { get; set; }
+
+        public int Serve()
+        {
+            return new Random().Next(1, 28);
+        }
     }
 }
