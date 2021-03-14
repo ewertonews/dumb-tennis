9fe3d3d1c96f257ca55b068c030ea7e657ac363b
Ewerton Willams da Silva <ewerton.willams@philips.com>
2021-03-13 22:02:43 -0300

    Refatoração parar passars método GetPlayerOneScore

--- EwsTennis/EwsTennis/ScoreBoard.cs
+++ EwsTennis/EwsTennis/ScoreBoard.cs
@@ -1,13 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace EwsTennis
 {
     public class ScoreBoard
     {
-        public List<int> ScoreList { get; } = new List<int>() { 0, 15, 30, 40 };
+        public List<int> ScoreList = new List<int>() { 0, 15, 30, 40 };
 
         public int PlayerOneScore { private get; set; }
         public int PlayerTwoScore { private get; set; }
 
+        public int GetPlayerOneScore()
+        {
+            return ScoreList[PlayerOneScore];
+        }
     }
 }
