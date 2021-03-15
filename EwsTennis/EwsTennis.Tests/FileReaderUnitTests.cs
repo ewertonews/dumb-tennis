953cd0b4ec9271235b33ec7f930eb05e55018f35
Ewerton Willams da Silva <ewerton.willams@philips.com>
2021-03-14 21:37:50 -0300

    Adicionando teste falho parar GetFileLines()

--- EwsTennis/EwsTennis.Tests/FileReaderUnitTests.cs
+++ EwsTennis/EwsTennis.Tests/FileReaderUnitTests.cs
@@ -19,5 +19,13 @@ namespace EwsTennis.Tests
             Assert.That(lines, Is.Not.Empty);
             Assert.That(lines.Count, Is.EqualTo(8));
         }
+
+        [Test]
+        public void GetFileLinesShouldThrowInvalidFileExceptionForIncompleteFile()
+        {
+            var fileReader = new FileReader("TestFiles/PlayersTestFileIncomplete.txt");
+
+            Assert.That(() => fileReader.GetFileLines(), Throws.TypeOf<InvalidFileException>());
+        }
     }
 }
