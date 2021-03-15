d8c70fe28bd58efd95c9f9fa05a833a34a28cb93
Ewerton Willams da Silva <ewerton.willams@philips.com>
2021-03-14 21:19:35 -0300

    Adicionando teste falho para TennisInputFileReader.GetPlayerTwoData()

--- EwsTennis/EwsTennis.Tests/TennisInputDataUnitTests.cs
+++ EwsTennis/EwsTennis.Tests/TennisInputDataUnitTests.cs
@@ -12,25 +12,39 @@ namespace EwsTennis.Tests
     {
         private TennisInputData _tennisInputFileReader;
         private Mock<IFileReader> _fileReaderMock;
+        private string[] _fakeFileLines;
+
         [SetUp]
         public void Setup()
         {
             _fileReaderMock = new Mock<IFileReader>();
             _tennisInputFileReader = new TennisInputData(_fileReaderMock.Object);
+            _fakeFileLines 
+                = new string[] { "Tonho", "Experienced", "Even", "25", "Zé", "Experienced", "Odd", "21" };
         }
 
         [Test]
         public void GetPlayerOneDataShouldReturnArrayOfStringWithLenghEquals4()
-        {
-            var fakeFileLines 
-                = new string[] { "Tonho", "Experienced", "Even", "25", "Zé", "Experienced", "Odd", "21" };
-            _fileReaderMock.Setup(fr => fr.GetFileLines()).Returns(fakeFileLines);
+        {               
+            _fileReaderMock.Setup(fr => fr.GetFileLines()).Returns(_fakeFileLines);
             var expectedPlayerData = new string[] { "Tonho", "Experienced", "Even", "25" };
 
             string[] playerOneData = _tennisInputFileReader.GetPlayerOneData();
 
             Assert.That(playerOneData.Length, Is.EqualTo(4));
-            Assert.That(playerOneData, Is.EquivalentTo(playerOneData));
+            Assert.That(playerOneData, Is.EqualTo(expectedPlayerData));
+        }
+
+        [Test]
+        public void GetPlayerTwoDataShouldReturnArrayOfStringWithLenghEquals4()
+        {
+            _fileReaderMock.Setup(fr => fr.GetFileLines()).Returns(_fakeFileLines);
+            var expectedPlayerData = new string[] { "Zé", "Experienced", "Odd", "21" };
+
+            string[] playerTwoData = _tennisInputFileReader.GetPlayerTwoData();
+
+            Assert.That(playerTwoData.Length, Is.EqualTo(4));
+            Assert.That(playerTwoData, Is.EquivalentTo(expectedPlayerData));
         }
     }
 }
